a45057ddc915c9612903721ec75c66261c8ba932
Nicole Wert <nicalexxandra@gmail.com>
2022-01-21 23:34:29 -0500

    #9 refactor: fix App.js Contact import

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home'; 
 import About from './components/About/About';
 import Projects from './components/Projects/Projects'; 
+import Contact from './components/Contact/Contact'; 
 
 function App() {
   return (
